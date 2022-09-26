77d5d0325871812462f4cc5a5e06a38fbcc4fe2f
Benrandja Akram <a.benrandja@gmail.com>
2022-09-26 19:35:32 +0100

    fix vercel build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack(config) {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        fabric: 'fabric-pure-browser',
+        ...config.resolve.alias,
+      },
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
